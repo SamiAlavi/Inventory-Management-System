e8f97be3f19bbd93c2d58952d37f76eb3e3c6d72
Sami Alavi <32700289+SamiAlavi@users.noreply.github.com>
2024-09-23 02:28:16 +0200

    fetch products

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import requests
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import os
 import socket
 
+load_dotenv()
+
 app = FastAPI()
+
+class Product(BaseModel):
+    product_name: str
+    stock_level: int
+    price: float
+
+@app.get("/fetch_products")
+async def fetch_products():
+    api_url = os.getenv('API_URL', 'https://fakestoreapi.com')
+    products_url = f'{api_url}/products'
+    response = requests.get(products_url)
+    if response.status_code == 200:
+        products: list[Product] = response.json()
+        for product in products:
+            print(product)
+        return {"message": "Products fetched and added to inventory successfully."}
+    else:
+        raise HTTPException(status_code=400, detail="Failed to fetch products from API")
 
 @app.get("/")
 async def home():
